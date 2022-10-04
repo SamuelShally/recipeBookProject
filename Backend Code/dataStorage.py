a6574b7a9105fcd894c846a4f8b583e0422eaaad
Shrey Mittal <shreymittal@gmail.com>
2022-10-04 14:46:12 -0400

    Exact string comparison integrated

--- Backend Code/dataStorage.py
+++ Backend Code/dataStorage.py
@@ -1,4 +1,5 @@
 #creating the preliminary backend solution for the project
+import pprint
 
 def add_recipe():
     # we will be using dictionaries as the storage method of choice
@@ -11,13 +12,53 @@ def add_recipe():
     #   }
     # }
 
-    recipe_name = input("What is the recipe called?")
+    recipe_name = input("What is the recipe called?: \n")
 
     recipes[recipe_name] = {
-                            "name" : "",
+                            "name" : recipe_name,
                             "description": "",
                             "ingredient_list": [],
                             "instructions" : []
                             }
 
+    recipes[recipe_name]["description"] = input("Give a description of the dish: \n")
+
+    anotherIngredient = "y"
+
+    while(anotherIngredient == "y"):
+        recipes[recipe_name]["ingredient_list"].append(input("Enter an ingredient: \n"))
+
+        anotherIngredient = input("Would you like to enter another ingredient (y/n): ")
+
+        # if anotherIngredient != "y" or anotherIngredient != "n":
+        #     print("Please enter y or n, as one character")
+        #     anotherIngredient = input("Would you like to enter another ingredient (y/n): ")
+
+
+
+
+    anotherInstruction = "y"
+
+    while (anotherInstruction == "y"):
+        recipes[recipe_name]["instructions"].append(input("Enter an instruction: \n"))
+
+        anotherInstruction = input("Would you like to enter another instruction (y/n): ")
+        #
+        # if anotherInstruction != "y" or anotherInstruction != "n":
+        #     print("Please enter y or n, as one character")
+        #     anotherInstruction = input("Would you like to enter another instruction (y/n): ")
+
     
+    pprint.pprint(recipes)
+
+    #given a string and list, returns the index of the string. If not, returns -1
+    def exact_string_comparison(inputVal, lst):
+        if(inputVal in lst):
+            return lst.index(inputVal)
+        else:
+            return -1
+
+    
+
+add_recipe()
+
